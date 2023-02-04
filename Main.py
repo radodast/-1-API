272ee0c7fc952bb9a7d0ea649aa5f85f68f8f5b2
flipstar <flip014@mail.ru>
2023-02-04 17:35:55 +0300

    fix

--- Main.py
+++ Main.py
@@ -7,54 +7,48 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
 from PyQt5 import uic
 
 
-class MyWidget(QMainWindow):
+
+
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('arbuz.ui', self)
+        self.cords = ['55.755864', '37.617698']
+        self.size = ['1', '1']
+        self.getImage()
+        self.initUI()
 
-# SCREEN_SIZE = [600, 450]
-#
-#
-# class Example(QWidget):
-#     def __init__(self):
-#         super().__init__()
-#         self.getImage()
-#         self.initUI()
-#
-#     def getImage(self):
-#         global cords, size
-#         map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(cords.split())}&spn={','.join(size.split())}&l=map"
-#         response = requests.get(map_request)
-#
-#         if not response:
-#             print("Ошибка выполнения запроса:")
-#             print(map_request)
-#             print("Http статус:", response.status_code, "(", response.reason, ")")
-#             sys.exit(1)
-#
-#         # Запишем полученное изображение в файл.
-#         self.map_file = "map.png"
-#         with open(self.map_file, "wb") as file:
-#             file.write(response.content)
-#
-#     def initUI(self):
-#         self.setGeometry(100, 100, *SCREEN_SIZE)
-#         self.setWindowTitle('Отображение карты')
-#
-#         ## Изображение
-#         self.pixmap = QPixmap(self.map_file)
-#         self.image = QLabel(self)
-#         self.image.move(0, 0)
-#         self.image.resize(600, 450)
-#         self.image.setPixmap(self.pixmap)
-#
-#     def closeEvent(self, event):
-#         """При закрытии формы подчищаем за собой"""
-#         os.remove(self.map_file)
+    def getImage(self):
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(self.cords)}&spn={','.join(self.size)}&l=map"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.setWindowTitle('Отображение карты')
+
+        ## Изображение
+        self.pixmap = QPixmap(self.map_file)
+        self.map.move(0, 0)
+        self.map.resize(600, 450)
+        self.map.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = Example()
     ex.show()
     sys.exit(app.exec_())
